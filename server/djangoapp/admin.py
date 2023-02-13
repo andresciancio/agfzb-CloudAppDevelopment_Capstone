edade4daa71de2efd34a38782aff43a6f927b794
Andres Ciancio <ajciancio@gmail.cm>
2023-02-13 15:14:40 -0300

    CarMake and CarModel models added

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
+from .models import CarMake,CarModel
 # from .models import related models
 
 
 # Register your models here.
 
 # CarModelInline class
+class carModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 3
+
 
 # CarModelAdmin class
 
+class CarModelAdmin (admin.ModelAdmin):
+    list_display=('name', 'cartype', 'year', 'carmake', 'dealer')
+    search_fields=['name', 'year', 'carmake']
+    list_filter = ['carmake']
+
 # CarMakeAdmin class with CarModelInline
 
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines=[carModelInline]
+    list_display=('name', 'description')
+
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,12 +5,29 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
+class CarMake(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000, default=" ")
+
+    def __str__(self):
+        return self.name 
+              
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
+    name = models.CharField(max_length=255)
+    cartype = models.CharField(max_length=255)
+    year = models.DateField(null=True)
+    dealer = models.IntegerField(null=False)
 
+    def __str__(self):
+        return self.name 
+               
+      
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
